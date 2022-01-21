3105954f2296ff21a74a1aeb93bff5ad3dc24f2c
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-01-21 02:57:13 +0000

    setting up qemu m3

--- app_zephyr_apis/src/main.nim
+++ app_zephyr_apis/src/main.nim
@@ -19,14 +19,26 @@ const
   SLEEP_TIME_MS* = 100
   MAX_SLEEP_MS* = 1_400
 
+testsZkFifo() # template to setup zk fifo tests 
+
 ##  The devicetree node identifier for the "led0" alias.
-testsZkFifo()
+const EMULATED_BOARDS = [
+  "native_posix",
+  "native_posix_64",
+  "qemu_cortex_m3",
+]
+
+when BOARD in ["qemu_cortex_m3"]:
+  import nephyr/randoms
+  proc z_impl_sys_csrand_get(dst: pointer, outlen: csize_t): cint {.exportc: "$1".}=
+    sys_rand_get(dst, outlen)
+    return 0
 
 app_main():
   logNotice("Booting main application:", VERSION)
   echo("starting app...")
 
-  when not BOARD.startsWith("native_posix"):
+  when not (BOARD in EMULATED_BOARDS):
     runTestPins()
 
   runAtomics()
