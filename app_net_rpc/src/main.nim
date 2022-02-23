ec8c072b3628ce49cbd10cf14e9d62d67670f232
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-02-23 23:32:18 +0000

    fixing comp issues

--- app_net_rpc/src/main.nim
+++ app_net_rpc/src/main.nim
@@ -1,8 +1,12 @@
 import std/os
 import std/monotimes
 
-import nephyr
 import mcu_utils/logging
+
+import nephyr
+import zephyr_c/net/znet_ip
+import zephyr_c/net/znet_if
+import zephyr_c/net/zipv6
 
 import fastrpc/server/fastrpcserver
 import fastrpc/server/rpcmethods
@@ -11,23 +15,48 @@ import rpc_server
 import version
 
 
-proc timePublisher*(params: (InetEventQueue[int64], int)) {.thread.} =
-  let 
-    queue = params[0]
-    delayMs = params[1]
-    n = 10
+proc join_coap_multicast_group*(): bool =
+  var mcast_addr: Sockaddr_in6
+  var ifaddr: ptr net_if_addr
 
-  while true:
-    var ts = int64(getMonoTime().ticks() div 1000)
-    # logDebug "timePublisher: ", "ts:", ts, "queue:len:", queue.chan.peek()
-    var qvals = isolate ts
-    discard queue.trySend(qvals)
-    os.sleep(delayMs)
+  var iface: ptr net_if = net_if_get_default()
+  if iface.isNil:
+    raise newException(Exception, "Could not get the default interface\n")
+
+  var my_addr: In6Addr
+  ifaddr = net_if_ipv6_addr_add(iface, addr(my_addr), NET_ADDR_MANUAL, 0)
+  if ifaddr.isNil:
+    logError("Could not add unicast address to interface")
+    return false
+  ifaddr.addr_state = NET_ADDR_PREFERRED
+  let ret = net_ipv6_mld_join(iface, addr(mcast_addr.sin6_addr))
+  if ret < 0:
+    logError("Cannot join IPv6 multicast group: ",
+            repr(mcast_addr.sin6_addr), " code: ", ret)
+    return false
+  return true
+
+proc find_ll_addr*() =
+  var mcast_addr: Sockaddr_in6
+  let iface: ptr net_if = net_if_get_default()
+  if iface.isNil:
+    raise newException(Exception, "Could not get the default interface\n")
+
+  echo "iface: ", repr(iface)
+
+  #  struct in6_addr *net_if_ipv6_get_ll(struct net_if *iface, enum net_addr_state addr_state)¶
+  let lladdr: ptr In6Addr = net_if_ipv6_get_ll(iface, NET_ADDR_ANY_STATE)
+  echo "ll addr: ", repr(lladdr)
+
+
 
 app_main():
   logNotice("Booting main application: " & VERSION)
 
   try:
+    ## ll addr
+    find_ll_addr()
+
     echo "setup timer thread"
     var
       timer1q = TimerDataQ.init(10)
